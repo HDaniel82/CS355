a89b8e05765aaea1e303fc0306b5c6a3795175f7
Daniel Huynh <150358797+HDaniel82@users.noreply.github.com>
2025-03-12 07:40:25 +0000

    completing main computation for the formula

--- Program1/program1.py
+++ Program1/program1.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import matploblib.pyplot as plt
-#computation for first run of [1-26] then [27-52]
+#computation for first run of [1-26] then [27-52] [daniel huynh]
 def shuffle(deck);
     #shuffle deck from 1-26 and 27-52 
     half1 =deck[:26]
@@ -9,8 +9,12 @@ def shuffle(deck);
     shuffled = [card for pair in zip(half1, half2) for card in pair]
     return shuffled
 
-#main computation for the given formula: 
+#main computation for the given formula: [daniel huynh]
 def compuation(deck); 
-
+n = 52; 
+compute_sum = sum((i+1) * deck[i] for i in range(n)) #sum of i * yi
+numer = (n* compute_sum) - 13782
+denom =  2507960 - 13782 
+return numer / denom
 
 def main()
