c4d33b086b6f57c975611cfa798a99649018ca21
Rizwan Khan <139818190+rakhan19@users.noreply.github.com>
2025-03-15 20:39:34 +0000

    r calculator function complete

--- Program1/rizprogram1.py
+++ Program1/rizprogram1.py
@@ -7,3 +7,11 @@ def cardShuffles(numOfCards):
     sumsq = 0
     for i in range(1, numOfCards+1):
         sumsq += i*i
+    sqsum = sumi*sumi
+
+    def rCalculator(deck):
+        sumIyi = 0
+        for i in range(numOfCards):
+            sumIyi += (i+1)*deck[i]
+        r= (numOfCards*sumIyi - sqsum)/(numOfCards*sumsq - sqsum)
+        return r
